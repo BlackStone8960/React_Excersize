2a920c81497d770d03a8678f6d286a8673effb02
BlackStone <fng.trf.kk1020@gmail.com>
2020-11-24 22:04:33 +0900

    Jest halfway through

--- expensify-app/src/tests/actions/filters.test.js
+++ expensify-app/src/tests/actions/filters.test.js
@@ -1,5 +1,11 @@
 import moment from 'moment';
-import { setStartDate, setEndDate, sortByAmount, sortByDate, setTextFilter } from '../../actions/filters';
+import { 
+	setStartDate, 
+	setEndDate, 
+	sortByAmount, 
+	sortByDate, 
+	setTextFilter 
+} from '../../actions/filters';
 
 test('should generate set start date action object', () => {
 		const action = setStartDate(moment(0));
@@ -17,14 +23,6 @@ test('should generate set end date action object', () => {
 		});
 });
 
-test('should generate sort by amount action object', () => {
-		expect(sortByAmount()).toEqual({ type: 'SORT_BY_AMOUNT' });
-});
-
-test('should generate sort by date action object',() => {
-		expect(sortByDate()).toEqual({ type: 'SORT_BY_DATE' });
-});
-
 test('should generate set text filter action object',() => {
 		const action = setTextFilter('Taichi');
 		expect(action).toEqual({
@@ -39,4 +37,12 @@ test('should generate set text filter action object provided no arguments', () =
 				type: 'SET_TEXT_FILTER',
 				text: ''
 		});
+});
+
+test('should generate sort by date action object',() => {
+	expect(sortByDate()).toEqual({ type: 'SORT_BY_DATE' });
+});
+
+test('should generate sort by amount action object', () => {
+	expect(sortByAmount()).toEqual({ type: 'SORT_BY_AMOUNT' });
 });
